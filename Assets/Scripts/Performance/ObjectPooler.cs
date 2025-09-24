5cbbeaf10114e47c98c63e296f983994fca69be6
Marmine15 <martinandreas2006@gmail.com>
2025-09-24 11:33:47 +0200

    object pool start

---
+++ Assets/Scripts/Performance/ObjectPooler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int amount;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.amount; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+}
+

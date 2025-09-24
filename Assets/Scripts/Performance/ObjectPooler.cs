26022cef5c8a87e1bf6c9507b03926df8869732c
Marmine15 <martinandreas2006@gmail.com>
2025-09-24 16:16:31 +0200

    object pool functionality

--- Assets/Scripts/Performance/ObjectPooler.cs
+++ Assets/Scripts/Performance/ObjectPooler.cs
@@ -11,6 +11,19 @@ public class ObjectPooler : MonoBehaviour
         public GameObject prefab;
         public int amount;
     }
+    
+    #region Singleton
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    
 
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
@@ -33,5 +46,23 @@ public class ObjectPooler : MonoBehaviour
         }
     }
 
-}
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn’t exist");
+            return null;
+        }
 
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+}

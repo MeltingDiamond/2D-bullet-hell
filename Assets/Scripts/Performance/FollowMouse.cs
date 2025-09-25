e472762a0f669e609d813b619d1c697dd4b95631
Marmine15 <martinandreas2006@gmail.com>
2025-09-25 12:12:47 +0200

    the mouse does something
    lmai

--- Assets/Scripts/Performance/FollowMouse.cs
+++ Assets/Scripts/Performance/FollowMouse.cs
@@ -3,18 +3,16 @@ using UnityEngine.InputSystem;
 
 public class FollowMouse : MonoBehaviour
 {
-    private Vector2 _mousePosition;
     private InputManager _inputSystem;
 
     private void Start()
     {
         _inputSystem = GetComponent<InputManager>();
-        _mousePosition = _inputSystem.MousePosition;
     }
 
     private void Update()
     {
-        var zRotation = Mathf.Atan2(_mousePosition.y, _mousePosition.x) *  Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, zRotation);
+        var zRotation = Mathf.Atan2(_inputSystem.MousePosition.y, _inputSystem.MousePosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, zRotation - 90f);
     }
 }
